f2b3fdc286df3d5850b7eb5520aeeb5b2e2d3620
dante1130 <andrewdante2@gmail.com>
2024-04-06 18:13:31 +0800

    Enable blending

--- src/Engine/Graphics/OpenGL/OpenGLRenderer.cpp
+++ src/Engine/Graphics/OpenGL/OpenGLRenderer.cpp
@@ -111,6 +111,9 @@ OpenGLRenderer::OpenGLRenderer(Window& window)
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LESS);
 
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
     glEnable(GL_CULL_FACE);
     glCullFace(GL_BACK);
     glFrontFace(GL_CW);
