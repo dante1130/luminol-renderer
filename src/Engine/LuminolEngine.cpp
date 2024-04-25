b215cdad20d8cbe300ac124ad1d6732c56522a18
dante1130 <andrewdante2@gmail.com>
2024-04-25 17:54:38 +0800

    format and remove dead code

--- src/Engine/LuminolEngine.cpp
+++ src/Engine/LuminolEngine.cpp
@@ -10,32 +10,6 @@ constexpr auto camera_initial_position = glm::vec3(5.0f, 0.0f, 0.0f);
 constexpr auto camera_initial_forward = glm::vec3(-1.0f, 0.0f, 0.0f);
 constexpr auto camera_rotation_speed = 0.1f;
 
-using namespace Luminol::Graphics;
-
-struct LightEntity {
-    glm::vec3 position;
-    glm::vec3 color;
-    std::unique_ptr<Model> model;
-};
-
-auto create_point_light(const glm::vec3& position, const glm::vec3& color)
-    -> PointLight {
-    constexpr auto specular_multiplier = 2.0f;
-    constexpr auto constant = 0.0f;
-    constexpr auto linear = 1.0f;
-    constexpr auto quadratic = 0.0f;
-
-    return PointLight{
-        .position = position,
-        .ambient = glm::vec3(0.0f),
-        .diffuse = color,
-        .specular = color * specular_multiplier,
-        .constant = constant,
-        .linear = linear,
-        .quadratic = quadratic
-    };
-}
-
 }  // namespace
 
 namespace Luminol {
@@ -51,21 +25,15 @@ Engine::Engine(const Properties& properties)
       )),
       renderer(this->graphics_factory->create_renderer(this->window)) {}
 
-auto Engine::get_window() const -> const Window& {
-    return this->window;
-}
+auto Engine::get_window() const -> const Window& { return this->window; }
 
-auto Engine::get_window() -> Window& {
-    return this->window;
-}
+auto Engine::get_window() -> Window& { return this->window; }
 
 auto Engine::get_camera() const -> const Graphics::Camera& {
     return this->camera;
 }
 
-auto Engine::get_camera() -> Graphics::Camera& {
-    return this->camera;
-}
+auto Engine::get_camera() -> Graphics::Camera& { return this->camera; }
 
 auto Engine::get_graphics_factory() const -> const Graphics::GraphicsFactory& {
     return *this->graphics_factory;
@@ -75,8 +43,6 @@ auto Engine::get_renderer() const -> const Graphics::Renderer& {
     return *this->renderer;
 }
 
-auto Engine::get_renderer() -> Graphics::Renderer& {
-    return *this->renderer;
-}
+auto Engine::get_renderer() -> Graphics::Renderer& { return *this->renderer; }
 
 }  // namespace Luminol
