1fa6f66b4e58813c50a66c4803b2fae22cad1e5d
dante1130 <andrewdante2@gmail.com>
2024-04-25 13:29:01 +0800

    put calculating tbn into vertex shader

--- res/shaders/gbuffer_frag.glsl
+++ res/shaders/gbuffer_frag.glsl
@@ -7,7 +7,7 @@ layout(location = 2) out vec4 gbuffer_albedo_spec;
 in vec2 tex_coords_out;
 in vec3 frag_pos_out;
 in vec3 normal_out;
-in vec3 tangent_out;
+in mat3 tangent_space_matrix_out;
 
 struct Material {
     sampler2D texture_diffuse;
@@ -18,19 +18,10 @@ struct Material {
 
 uniform Material material;
 
-vec3 calculate_normal(sampler2D material_texture_normal, vec3 normal, vec3 tangent, vec2 tex_coords)
+vec3 calculate_normal(sampler2D material_texture_normal, vec2 tex_coords, mat3 tbn)
 {
     vec3 normal_map = texture(material_texture_normal, tex_coords).rgb;
     normal_map = normalize(normal_map * 2.0 - 1.0);
-
-    const vec3 n = normalize(normal);
-
-    vec3 t = normalize(tangent);
-    t = normalize(t - dot(t, n) * n);
-
-    const vec3 b = cross(n, t);
-
-    const mat3 tbn = mat3(t, b, n);
 
     return normalize(tbn * normal_map);
 }
@@ -38,7 +29,7 @@ vec3 calculate_normal(sampler2D material_texture_normal, vec3 normal, vec3 tange
 void main()
 {
     gbuffer_position = vec4(frag_pos_out, 1.0);
-    gbuffer_normal = vec4(calculate_normal(material.texture_normal, normal_out, tangent_out, tex_coords_out), 1.0);
+    gbuffer_normal = vec4(calculate_normal(material.texture_normal, tex_coords_out, tangent_space_matrix_out), 1.0);
     gbuffer_albedo_spec.rgb = texture(material.texture_diffuse, tex_coords_out).rgb + texture(material.texture_emissive, tex_coords_out).rgb;
     gbuffer_albedo_spec.a = texture(material.texture_specular, tex_coords_out).r;
 }
--- res/shaders/gbuffer_vert.glsl
+++ res/shaders/gbuffer_vert.glsl
@@ -8,7 +8,7 @@ layout(location = 3) in vec3 tangent;
 out vec2 tex_coords_out;
 out vec3 frag_pos_out;
 out vec3 normal_out;
-out vec3 tangent_out;
+out mat3 tangent_space_matrix_out;
 
 layout(std140, binding = 0) uniform Transform
 {
@@ -23,5 +23,10 @@ void main()
     frag_pos_out = vec3(model_matrix * vec4(position, 1.0));
     tex_coords_out = tex_coords;
     normal_out = mat3(transpose(inverse(model_matrix))) * normal;
-    tangent_out = mat3(transpose(inverse(model_matrix))) * tangent;
+
+    vec3 T = normalize(vec3(model_matrix * vec4(tangent, 0.0)));
+    vec3 N = normalize(vec3(model_matrix * vec4(normal, 0.0)));
+    T = normalize(T - dot(T, N) * N);
+    vec3 B = cross(N, T);
+    tangent_space_matrix_out = mat3(T, B, N);
 }
