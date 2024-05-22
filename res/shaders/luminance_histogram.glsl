0a10b28489d55df21aa2148018d7444b04348452
dante1130 <andrewdante2@gmail.com>
2024-05-23 00:25:33 +0800

    Calculate luminance histogram

--- res/shaders/luminance_histogram.glsl
+++ res/shaders/luminance_histogram.glsl
@@ -4,17 +4,36 @@ layout(rgba16f, binding = 1) uniform image2D hdr_image;
 
 const uint group_size = gl_WorkGroupSize.x * gl_WorkGroupSize.y;
 
-const vec3 rgb_to_luminance = vec3(0.2126, 0.7152, 0.0722);
-
 layout(std430, binding = 2) buffer LuminanceHistogramBuffer
 {
     uint luminance_histogram[group_size];
 };
 
+uint color_to_bin(vec3 color, float min_log_luminance, float inverse_log_luminance_range) {
+    const vec3 rgb_to_luminance = vec3(0.2126, 0.7152, 0.0722);
+    const float luminance = dot(color, rgb_to_luminance);
+
+    const float epsilon = 0.005;
+
+    if (luminance < epsilon) {
+        return 0;
+    }
+
+    const float normalized_log_luminance = clamp((log2(luminance) - min_log_luminance) * inverse_log_luminance_range, 0.0, 1.0);
+
+    return uint(normalized_log_luminance * 254.0 + 1.0);
+}
+
 void main() {
-    const ivec2 pixel_coords = ivec2(gl_GlobalInvocationID.xy);
-    const vec4 color = imageLoad(hdr_image, pixel_coords);
-    const float luminance = dot(color.rgb, rgb_to_luminance);
-    const uint bin = uint(luminance * float(group_size));
+    const ivec2 texel = ivec2(gl_GlobalInvocationID.xy);
+
+    const vec3 color = imageLoad(hdr_image, texel).rgb;
+
+    const float min_log_luminance = -10.0;
+    const float max_log_luminance = 2.0;
+    const float inverse_log_luminance_range = 1.0 / (max_log_luminance - min_log_luminance);
+
+    const uint bin = color_to_bin(color, min_log_luminance, inverse_log_luminance_range);
+
     atomicAdd(luminance_histogram[bin], 1);
 }
