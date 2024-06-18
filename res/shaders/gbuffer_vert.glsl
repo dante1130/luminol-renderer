723d5dea912f833021c74968e0e32fe3821a618b
dante1130 <andrewdante2@gmail.com>
2024-06-18 22:26:52 +0800

    Fix Gbuffer not using instanced model matrix buffer

--- res/shaders/gbuffer_vert.glsl
+++ res/shaders/gbuffer_vert.glsl
@@ -24,13 +24,14 @@ layout(std430, binding = 0) buffer InstancingModelMatrix
 
 void main()
 {
-    gl_Position = projection_matrix * view_matrix * model_matrices[gl_InstanceID] * vec4(position, 1.0);
-    frag_pos_out = vec3(model_matrix * vec4(position, 1.0));
+    mat4 instanced_model_matrix = model_matrices[gl_InstanceID];
+    gl_Position = projection_matrix * view_matrix * instanced_model_matrix * vec4(position, 1.0);
+    frag_pos_out = vec3(instanced_model_matrix * vec4(position, 1.0));
     tex_coords_out = tex_coords;
-    normal_out = mat3(transpose(inverse(model_matrix))) * normal;
+    normal_out = mat3(transpose(inverse(instanced_model_matrix))) * normal;
 
-    vec3 T = normalize(vec3(model_matrix * vec4(tangent, 0.0)));
-    vec3 N = normalize(vec3(model_matrix * vec4(normal, 0.0)));
+    vec3 T = normalize(vec3(instanced_model_matrix * vec4(tangent, 0.0)));
+    vec3 N = normalize(vec3(instanced_model_matrix * vec4(normal, 0.0)));
     T = normalize(T - dot(T, N) * N);
     vec3 B = cross(N, T);
     tangent_space_matrix_out = mat3(T, B, N);
