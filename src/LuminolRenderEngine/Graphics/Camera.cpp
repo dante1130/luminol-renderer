d57546b090c221df8ffcb937e8169586d1ac5dd3
dante1130 <andrewdante2@gmail.com>
2024-11-07 14:05:27 +0800

    Transition Camera to use LuminolMaths::Units

--- src/LuminolRenderEngine/Graphics/Camera.cpp
+++ src/LuminolRenderEngine/Graphics/Camera.cpp
@@ -18,19 +18,18 @@ Camera::Camera(const CameraProperties& properties)
       forward(properties.forward.normalized()),
       right_vector(this->forward.cross(up_vector_world).normalized()),
       up_vector(this->right_vector.cross(this->forward).normalized()),
-      fov_degrees(properties.fov_degrees),
+      fov(properties.fov),
       aspect_ratio(properties.aspect_ratio),
       near_plane(properties.near_plane),
       far_plane(properties.far_plane),
       translation_speed(properties.translation_speed),
       rotation_speed(properties.rotation_speed),
-      yaw_degrees(glm::degrees(std::atan2(this->forward.x(), this->forward.z()))
-      ),
-      pitch_degrees(glm::degrees(std::asin(this->forward.y()))) {}
+      yaw(glm::degrees(std::atan2(this->forward.x(), this->forward.z()))),
+      pitch(glm::degrees(std::asin(this->forward.y()))) {}
 
-auto Camera::move(CameraMovement direction, float delta_time_in_seconds)
+auto Camera::move(CameraMovement direction, Units::Seconds_f delta_time)
     -> void {
-    const auto velocity = this->translation_speed * delta_time_in_seconds;
+    const auto velocity = this->translation_speed * delta_time.get_value();
 
     switch (direction) {
         case CameraMovement::Forward:
@@ -48,24 +47,29 @@ auto Camera::move(CameraMovement direction, float delta_time_in_seconds)
     }
 }
 
-auto Camera::rotate(float yaw_degrees, float pitch_degrees) -> void {
-    this->yaw_degrees += yaw_degrees * this->rotation_speed;
-    this->pitch_degrees += pitch_degrees * this->rotation_speed;
+auto Camera::rotate(Units::Degrees_f yaw, Units::Degrees_f pitch) -> void {
+    this->yaw += yaw * this->rotation_speed;
+    this->pitch += pitch * this->rotation_speed;
 
-    constexpr auto max_yaw_degrees = 360.0f;
-    constexpr auto max_pitch_degrees = 89.0f;
-    constexpr auto min_pitch_degrees = -89.0f;
+    constexpr auto max_yaw_degrees = Units::Degrees_f{360.0f};
+    constexpr auto max_pitch_degrees = Units::Degrees_f{89.0f};
+    constexpr auto min_pitch_degrees = Units::Degrees_f{-89.0f};
 
-    this->yaw_degrees = std::fmod(this->yaw_degrees, max_yaw_degrees);
-    this->pitch_degrees =
-        std::clamp(this->pitch_degrees, min_pitch_degrees, max_pitch_degrees);
+    this->yaw = std::fmod(this->yaw.get_value(), max_yaw_degrees.get_value());
+    this->pitch = std::clamp(
+        this->pitch.get_value(),
+        min_pitch_degrees.get_value(),
+        max_pitch_degrees.get_value()
+    );
 
-    const auto yaw_radians = glm::radians(this->yaw_degrees);
-    const auto pitch_radians = glm::radians(this->pitch_degrees);
+    const auto yaw_radians = this->yaw.as<Units::Radian>();
+    const auto pitch_radians = this->pitch.as<Units::Radian>();
 
-    this->forward.x() = std::sinf(yaw_radians) * std::cosf(pitch_radians);
-    this->forward.y() = std::sinf(pitch_radians);
-    this->forward.z() = std::cosf(yaw_radians) * std::cosf(pitch_radians);
+    this->forward.x() = std::sinf(yaw_radians.get_value()) *
+                        std::cosf(pitch_radians.get_value());
+    this->forward.y() = std::sinf(pitch_radians.get_value());
+    this->forward.z() = std::cosf(yaw_radians.get_value()) *
+                        std::cosf(pitch_radians.get_value());
 
     this->forward = this->forward.normalized();
 
@@ -87,7 +91,7 @@ auto Camera::get_view_matrix() const -> Maths::Matrix4x4f {
 auto Camera::get_projection_matrix() const -> Maths::Matrix4x4f {
     return Maths::Transform::left_handed_perspective_projection_matrix(
         Maths::Transform::PerspectiveMatrixParams<float>{
-            .fov = this->fov_degrees,
+            .fov = this->fov,
             .aspect_ratio = this->aspect_ratio,
             .near_plane = this->near_plane,
             .far_plane = this->far_plane
@@ -115,9 +119,7 @@ auto Camera::set_position(const Maths::Vector3f& position) -> void {
     this->position = position;
 }
 
-auto Camera::set_fov(float fov_degrees) -> void {
-    this->fov_degrees = fov_degrees;
-}
+auto Camera::set_fov(Units::Degrees_f fov) -> void { this->fov = fov; }
 
 auto Camera::set_aspect_ratio(float aspect_ratio) -> void {
     this->aspect_ratio = aspect_ratio;
--- src/LuminolRenderEngine/Graphics/Camera.hpp
+++ src/LuminolRenderEngine/Graphics/Camera.hpp
@@ -1,8 +1,9 @@
 #pragma once
 
-#include <glm/glm.hpp>
 #include <LuminolMaths/Vector.hpp>
 #include <LuminolMaths/Matrix.hpp>
+#include <LuminolMaths/Units/Time.hpp>
+#include <LuminolMaths/Units/Angle.hpp>
 
 namespace Luminol::Graphics {
 
@@ -13,7 +14,7 @@ enum class CameraMovement {
     Right,
 };
 
-constexpr static auto default_fov_degrees = 45.0f;
+constexpr static auto default_fov = Units::Degrees_f{45.0f};
 constexpr static auto default_aspect_ratio = 1.0f;
 constexpr static auto default_near_plane = 0.1f;
 constexpr static auto default_far_plane = 100.0f;
@@ -23,7 +24,7 @@ struct CameraProperties {
     Maths::Vector3f forward = {0.0f, 0.0f, 1.0f};
     Maths::Vector3f up_vector = {0.0f, 1.0f, 0.0f};
 
-    float fov_degrees = default_fov_degrees;
+    Units::Degrees_f fov = default_fov;
     float aspect_ratio = default_aspect_ratio;
     float near_plane = default_near_plane;
     float far_plane = default_far_plane;
@@ -36,8 +37,8 @@ class Camera {
 public:
     Camera(const CameraProperties& properties);
 
-    auto move(CameraMovement direction, float delta_time) -> void;
-    auto rotate(float yaw_degrees, float pitch_degrees) -> void;
+    auto move(CameraMovement direction, Units::Seconds_f delta_time) -> void;
+    auto rotate(Units::Degrees_f yaw, Units::Degrees_f pitch) -> void;
 
     [[nodiscard]] auto get_view_matrix() const -> Maths::Matrix4x4f;
     [[nodiscard]] auto get_projection_matrix() const -> Maths::Matrix4x4f;
@@ -48,7 +49,7 @@ public:
     [[nodiscard]] auto get_right_vector() const -> const Maths::Vector3f&;
 
     auto set_position(const Maths::Vector3f& position) -> void;
-    auto set_fov(float fov_degrees) -> void;
+    auto set_fov(Units::Degrees_f fov) -> void;
     auto set_aspect_ratio(float aspect_ratio) -> void;
     auto set_near_plane(float near_plane) -> void;
     auto set_far_plane(float far_plane) -> void;
@@ -61,7 +62,7 @@ private:
     Maths::Vector3f right_vector = {1.0f, 0.0f, 0.0f};
     Maths::Vector3f up_vector = {0.0f, 1.0f, 0.0f};
 
-    float fov_degrees = default_fov_degrees;
+    Units::Degrees_f fov = default_fov;
     float aspect_ratio = default_aspect_ratio;
     float near_plane = default_near_plane;
     float far_plane = default_far_plane;
@@ -69,8 +70,8 @@ private:
     float translation_speed = 1.0f;
     float rotation_speed = 1.0f;
 
-    float yaw_degrees = 0.0f;
-    float pitch_degrees = 0.0f;
+    Units::Degrees_f yaw = 0.0f;
+    Units::Degrees_f pitch = 0.0f;
 };
 
 }  // namespace Luminol::Graphics
