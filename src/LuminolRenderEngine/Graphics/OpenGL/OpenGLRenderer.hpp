0de6cb1cd12d0e325b3adcae45a050f1e2137e5b
Andrew <andrew_ho_sy@proton.me>
2026-04-22 14:50:37 +0800

    Revert "Revert "Luminance heatmap overlay""

--- src/LuminolRenderEngine/Graphics/OpenGL/OpenGLRenderer.hpp
+++ src/LuminolRenderEngine/Graphics/OpenGL/OpenGLRenderer.hpp
@@ -11,6 +11,7 @@
 #include <LuminolRenderEngine/Graphics/OpenGL/OpenGLSkyboxRenderPass.hpp>
 #include <LuminolRenderEngine/Graphics/OpenGL/OpenGLHDRRenderPass.hpp>
 #include <LuminolRenderEngine/Graphics/OpenGL/OpenGLAutoExposureRenderPass.hpp>
+#include <LuminolRenderEngine/Graphics/OpenGL/OpenGLLuminanceHeatmapRenderPass.hpp>
 #include <LuminolRenderEngine/Graphics/OpenGL/OpenGLDrawCall.hpp>
 
 namespace Luminol::Graphics {
@@ -24,6 +25,8 @@ public:
         -> void override;
 
     auto set_exposure(float exposure) -> void override;
+    auto set_luminance_heatmap_enabled(bool enabled) -> void override;
+    [[nodiscard]] auto get_luminance_heatmap_enabled() const -> bool override;
 
     auto clear_color(const Maths::Vector4f& color) const -> void override;
     auto clear(BufferBit buffer_bit) const -> void override;
@@ -73,6 +76,7 @@ private:
     OpenGLSkyboxRenderPass skybox_render_pass;
     OpenGLHDRRenderPass hdr_render_pass;
     OpenGLAutoExposureRenderPass auto_exposure_render_pass;
+    OpenGLLuminanceHeatmapRenderPass luminance_heatmap_render_pass;
 
     OpenGLUniformBuffer transform_uniform_buffer;
     OpenGLUniformBuffer light_uniform_buffer;
@@ -80,6 +84,7 @@ private:
     OpenGLShaderStorageBuffer instancing_color_buffer;
 
     float exposure = 1.0f;
+    bool luminance_heatmap_enabled = false;
 
     Maths::Matrix4x4f view_matrix;
     Maths::Matrix4x4f projection_matrix;
